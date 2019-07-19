b9bf5e110c5bca901d68f74447a56c937cfb998f
okiki <okiki.oyelaw@gmail.com>
2019-07-20 00:14:59 +0100

    create backdrop component

---
+++ src/components/BackDrop.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="backdrop">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BackDrop"
+};
+</script>
+
+<style scoped>
+.backdrop {
+  background: #dde2e9;
+  height: 30vh;
+}
+</style>
