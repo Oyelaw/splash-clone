e195a2add553b2898dd2f0ef1025c4194c13a7e4
okiki <okiki.oyelaw@gmail.com>
2019-07-20 09:54:15 +0100

    create search result header and added conditional statement for search result

---
+++ src/components/SearchHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <BackDrop>
+    <slot></slot>
+  </BackDrop>
+</template>
+
+<script>
+import BackDrop from "./BackDrop";
+
+export default {
+  name: "SearchHeader",
+  components: {
+    BackDrop
+  }
+};
+</script>
--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <h3>Search results screen</h3>
+    <SearchHeader>
+      <div
+        v-if="this.status === 'pending'"
+        class="search-header"
+      >Searching for '{{this.searchParam}}'</div>
+      <div
+        v-else-if="this.status === 'loaded'"
+        class="search-header"
+      >Search Results for '{{this.searchParam}}'</div>
+      <div v-else-if="this.status === 'Error'" class="search-header">
+        Unable to find results for
+        <span class="key-word">'{{this.searchParam}}'</span>. Check your internet connection
+      </div>
+    </SearchHeader>
     <PhotosList />
   </div>
 </template>
@@ -9,10 +22,12 @@
 import { mapActions, mapState } from "vuex";
 import Header from "../components/Header";
 import PhotosList from "../components/PhotoList";
+import SearchHeader from "../components/SearchHeader";
 export default {
   name: "SearchResult",
   components: {
-    PhotosList
+    PhotosList,
+    SearchHeader
   },
   methods: {
     ...mapActions("Photos", ["fetchPhotos"])
@@ -24,10 +39,22 @@ export default {
     this.fetchPhotos({ query });
   },
   computed: {
-    ...mapState("Photos", ["searchParam"])
+    ...mapState("Photos", ["searchParam", "status"])
   }
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.search-header {
+  padding: 30px 100px;
+  align-items: center;
+  height: 60px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+}
+.key-word {
+  font-weight: bold;
+  margin-left: 4px;
+}
 </style>
